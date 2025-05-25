3e67a8d8ebcd9d52dc162fe7af6f5a1d57aa02e7
PhaniPendela <krishnachaitanya.p@students.iiit.ac.in>
2025-05-25 16:35:20 +0530

    Finished 8th Folder Work

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    
+    function printAge () {
+        let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millinial = true;
+            const firstName = 'Steven';
+            const str = `Oh, and you're a millinial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+
+            output = 'NEW OUTPUT!';
+        }
+        // console.log(str);
+        console.log(millinial);
+        // add(2,3) //For sctrict mode it is also block made
+        console.log(output);
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
