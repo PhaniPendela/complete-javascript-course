02c0fcb01fd91054fd9efa9246e571153e788267
PhaniPendela <krishnachaitanya.p@students.iiit.ac.in>
2025-05-27 08:53:38 +0530

    9th Folder Logical Operators Next Level

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -70,8 +70,41 @@ const restaurant = {
   },
 };
 
+/*
+///////////////////////////////////////////
+// Logical AND OR Nullish Operators
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
 
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
 
+//Logical OR Operator
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Logical Nullish Operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// Logical AND Operator only if truthy then assigns
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+*/
 /*
 /////////////////////////////////////////////////
 // The Nullish Operator
