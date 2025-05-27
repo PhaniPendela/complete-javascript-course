7c608e8d2d33585dedc43798eb156e2ed2175ef8
PhaniPendela <krishnachaitanya.p@students.iiit.ac.in>
2025-05-27 09:10:53 +0530

    9th FOlder Challenge 1 till task 5

--- 09-Data-Structures-Operators/starter/challenge1.js
+++ 09-Data-Structures-Operators/starter/challenge1.js
@@ -37,5 +37,30 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
+  printGoals: function (...playerNames) {
+    console.log(playerNames);
+    // for (let i = 1; i <= playerNames.length(); i++) {
+    //     console.log(playerNames[i]);
+    // }
+    // console.log(playerNames.length());
+  },
 };
 
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+
+const allplayers = [...players1, ...players2];
+console.log(allplayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const {team1, x: draw, team2} = game.odds;
+console.log(team1, draw, team2);
+
+game.printGoals('Thiago', 'Coutinho', 'Perisic', 'Neuer', 'Pavard');
