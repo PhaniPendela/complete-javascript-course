113705eed1e899620e4909367bfcc450fc023209
PhaniPendela <krishnachaitanya.p@students.iiit.ac.in>
2025-05-27 12:23:58 +0530

    9th Folder Challenge 2 Tasks done

--- 09-Data-Structures-Operators/starter/challenge2.js
+++ 09-Data-Structures-Operators/starter/challenge2.js
@@ -42,3 +42,23 @@ const game = {
 };
 
 
+// const entries = game.scored;
+// console.log(entries);
+
+for (const [i, name] of game.scored.entries()) {
+    console.log(`Goal ${i + 1}: ${name}`);
+}
+
+// const values = Object.values(game.odds);
+// console.log(values);
+
+let sum = 0;
+for (const x of Object.values(game.odds)) {
+    sum += x;
+}
+let avg = sum/Object.values(game.odds).length;
+console.log(avg)
+
+for (const [key, value] of Object.entries(game.odds)) {
+    (key != 'x') ? console.log(`Odd of victory ${game[key]}: ${value}`) : console.log(`Odd of draw: ${game.odds.x}`);
+}
