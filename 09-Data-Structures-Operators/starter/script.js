b5da2f3b1cee5df9a534d9d234babfcc2443f1b6
Pendela V.P.B Krishna Chaitanya <67474140+PhaniPendela@users.noreply.github.com>
2025-05-31 08:13:09 +0530

    9th Folder Maps Iteration and accessing contents

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -72,6 +72,47 @@ const restaurant = {
   },
 };
 /*
+/////////////////////////////////////////////////////////////////////////
+// Maps Iteration
+const question  = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try Again!'],
+]);
+
+console.log(question);
+
+console.log(Object.entries(openingHours));
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key == 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+// const ans = Number(prompt('Your Answer'));
+const ans = 3;
+console.log(ans);
+
+console.log(question.get(ans == question.get('correct')));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+*/
+
+/*
 ///////////////////////////////////////////////////////////////////////////
 // Maps Introduction
 const rest = new Map();
