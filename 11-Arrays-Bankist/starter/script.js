67421a7e7c4f863edea248d0a5cfc9044457d9bc
Pendela V.P.B Krishna Chaitanya <67474140+PhaniPendela@users.noreply.github.com>
2025-06-12 17:06:37 +0530

    11th FOlder Foreach menthod on maps and sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -146,5 +146,24 @@ movements.forEach(function (mov, i, arr) {
       `Movement ${i + 1}: You withdrew ${Math.abs(mov)} amount from the bank.`
     );
   }
+});
+*/
+/*
+///////////////////////////////////////////
+// FOREACH for MAPS AND SETS
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, mpp) {
+  console.log(`${key}: ${value}`);
+});
+
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'USD', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
 });
 */
