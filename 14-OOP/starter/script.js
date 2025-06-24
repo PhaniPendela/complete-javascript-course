2f6022822fb650cad88be80944b86dfb618ff0bd
Pendela V.P.B Krishna Chaitanya <67474140+PhaniPendela@users.noreply.github.com>
2025-06-24 13:56:43 +0530

    14th Folder Started OOP and finished Challenge 1 learned Prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,110 @@
 'use strict';
+
+/*
+const Person = function (firstName, birthYear) {
+  // Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+// 1. New {} is created
+// 2. Function is called and this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// Object.prototype (top of the chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 5, 6, 9, 9, 8, 6, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+// Experiment Dont do it actually on builtin
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+*/
+/*
+///////////////////////////////////////////////
+// Challenge #1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  this.logSpeed();
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  this.logSpeed();
+};
+Car.prototype.logSpeed = function () {
+  console.log(`Current Speed of ${this.make} is ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.brake();
+bmw.brake();
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.brake();
+*/
